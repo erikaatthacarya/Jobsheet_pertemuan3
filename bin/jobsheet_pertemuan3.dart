ab8c8899a639208033dfc3f6af2c1d4660332baa
erikaatthacarya <erikaputric48@gmail.com>
2025-09-25 00:20:51 +0700

    new project

---
+++ bin/jobsheet_pertemuan3.dart
@@ -0,0 +1,5 @@
+import 'package:jobsheet_pertemuan3/jobsheet_pertemuan3.dart' as jobsheet_pertemuan3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jobsheet_pertemuan3.calculate()}!');
+}
