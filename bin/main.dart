cb7b89c27dab88b8bfdf8fa0d6a58ba2fac1cbc2
erikaatthacarya <erikaputric48@gmail.com>
2025-09-25 01:28:01 +0700

    add menu switch

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
 
+int faktorial(int n) {
+  if (n <= 1) return 1;
+  return n * faktorial(n - 1);
+}
+
 bool isGenap(int n) => n % 2 == 0;
 
 bool isPrima(int n) {
@@ -11,14 +16,41 @@ bool isPrima(int n) {
 }
 
 void main() {
-  stdout.write("Masukkan sebuah bilangan: ");
-  int angka = int.parse(stdin.readLineSync()!);
+  while (true) {
+    print("\n=== MENU PERHITUNGAN ===");
+    print("1. Hitung Faktorial");
+    print("2. Cek Bilangan Genap/Ganjil");
+    print("3. Cek Bilangan Prima");
+    print("4. Keluar");
 
-  print(isGenap(angka)
-      ? "$angka adalah bilangan genap"
-      : "$angka adalah bilangan ganjil");
+    stdout.write("Pilih menu: ");
+    int pilih = int.parse(stdin.readLineSync()!);
 
-  print(isPrima(angka)
-      ? "$angka adalah bilangan prima"
-      : "$angka bukan bilangan prima");
+    switch (pilih) {
+      case 1:
+        stdout.write("Masukkan angka: ");
+        int n = int.parse(stdin.readLineSync()!);
+        print("Faktorial dari $n = ${faktorial(n)}");
+        break;
+      case 2:
+        stdout.write("Masukkan angka: ");
+        int n = int.parse(stdin.readLineSync()!);
+        print(isGenap(n)
+            ? "$n adalah bilangan genap"
+            : "$n adalah bilangan ganjil");
+        break;
+      case 3:
+        stdout.write("Masukkan angka: ");
+        int n = int.parse(stdin.readLineSync()!);
+        print(isPrima(n)
+            ? "$n adalah bilangan prima"
+            : "$n bukan bilangan prima");
+        break;
+      case 4:
+        print("Program selesai, terima kasih!");
+        return;
+      default:
+        print("Pilihan tidak valid!");
+    }
+  }
 }
